b8931af4ac5641a8ebc120d7416b10b6e1967fbb
jenkins <ci-bot@angularjs.org>
2014-09-09 02:51:09 -0700

    v1.2.24-build.441+sha.5061d2c

--- angular-locale_es-mx.js
+++ angular-locale_es-mx.js
@@ -31,32 +31,32 @@ $provide.value("$locale", {
       "diciembre"
     ],
     "SHORTDAY": [
-      "dom.",
-      "lun.",
-      "mar.",
-      "mi\u00e9r.",
-      "jue.",
-      "vier.",
+      "dom",
+      "lun",
+      "mar",
+      "mi\u00e9",
+      "jue",
+      "vie",
       "s\u00e1b"
     ],
     "SHORTMONTH": [
-      "ene.",
-      "febr.",
-      "mzo.",
-      "abr.",
-      "my.",
-      "jun.",
-      "jul.",
-      "ag.",
-      "set.",
-      "oct.",
-      "nov.",
-      "dic."
+      "ene",
+      "feb",
+      "mar",
+      "abr",
+      "may",
+      "jun",
+      "jul",
+      "ago",
+      "sep",
+      "oct",
+      "nov",
+      "dic"
     ],
     "fullDate": "EEEE, d 'de' MMMM 'de' y",
     "longDate": "d 'de' MMMM 'de' y",
-    "medium": "dd/MM/y HH:mm:ss",
-    "mediumDate": "dd/MM/y",
+    "medium": "dd/MM/yyyy HH:mm:ss",
+    "mediumDate": "dd/MM/yyyy",
     "mediumTime": "HH:mm:ss",
     "short": "dd/MM/yy HH:mm",
     "shortDate": "dd/MM/yy",
@@ -94,6 +94,6 @@ $provide.value("$locale", {
     ]
   },
   "id": "es-mx",
-  "pluralCat": function (n, opt_precision) {  if (n == 1) {    return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}
+  "pluralCat": function (n) {  if (n == 1) {   return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}
 });
 }]);
