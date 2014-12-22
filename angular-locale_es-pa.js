131269725cb81e08241acbfa74afd72f5b387da3
jenkins <ci-bot@angularjs.org>
2014-12-22 08:07:44 -0800

    v1.2.29-build.584+sha.b193535

--- angular-locale_es-pa.js
+++ angular-locale_es-pa.js
@@ -4,8 +4,8 @@ var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "
 $provide.value("$locale", {
   "DATETIME_FORMATS": {
     "AMPMS": [
-      "a.\u00a0m.",
-      "p.\u00a0m."
+      "a. m.",
+      "p. m."
     ],
     "DAY": [
       "domingo",
@@ -25,7 +25,7 @@ $provide.value("$locale", {
       "junio",
       "julio",
       "agosto",
-      "setiembre",
+      "septiembre",
       "octubre",
       "noviembre",
       "diciembre"
@@ -48,24 +48,24 @@ $provide.value("$locale", {
       "jun.",
       "jul.",
       "ago.",
-      "set.",
+      "sept.",
       "oct.",
       "nov.",
       "dic."
     ],
     "fullDate": "EEEE, d 'de' MMMM 'de' y",
     "longDate": "d 'de' MMMM 'de' y",
-    "medium": "MM/dd/y h:mm:ss a",
+    "medium": "MM/dd/y H:mm:ss",
     "mediumDate": "MM/dd/y",
-    "mediumTime": "h:mm:ss a",
-    "short": "MM/dd/yy h:mm a",
+    "mediumTime": "H:mm:ss",
+    "short": "MM/dd/yy H:mm",
     "shortDate": "MM/dd/yy",
-    "shortTime": "h:mm a"
+    "shortTime": "H:mm"
   },
   "NUMBER_FORMATS": {
     "CURRENCY_SYM": "B/.",
-    "DECIMAL_SEP": ".",
-    "GROUP_SEP": ",",
+    "DECIMAL_SEP": ",",
+    "GROUP_SEP": ".",
     "PATTERNS": [
       {
         "gSize": 3,
@@ -84,14 +84,14 @@ $provide.value("$locale", {
         "maxFrac": 2,
         "minFrac": 2,
         "minInt": 1,
-        "negPre": "\u00a4-",
-        "negSuf": "",
-        "posPre": "\u00a4",
-        "posSuf": ""
+        "negPre": "-",
+        "negSuf": "\u00a0\u00a4",
+        "posPre": "",
+        "posSuf": "\u00a0\u00a4"
       }
     ]
   },
   "id": "es-pa",
-  "pluralCat": function(n, opt_precision) {  if (n == 1) {    return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}
+  "pluralCat": function (n, opt_precision) {  if (n == 1) {    return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}
 });
 }]);
